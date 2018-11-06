0094e1680c2f4fe87e7458500b1c42d84d02b1e6
Daltz333 <gamefollower26@gmail.com>
2018-11-06 15:53:07 -0500

    Update state machine logic

--- src/main/java/logic/MultiplayerStates.java
+++ src/main/java/logic/MultiplayerStates.java
@@ -1,16 +1,45 @@
 package logic;
 
+import scenes.BoardScene;
+import sockets.Server;
+
 public class MultiplayerStates {
 
-    public void setGameState(String dataType) {
+    public void handleClientData(String data) {
+        String result = "Current data is: " + data + "\n";
+        String dataType = null;
+        String dataContent = null;
+
+        System.out.println("Data is: " + result);
+
+        try {
+            dataType = data.substring(data.indexOf("["), data.indexOf("]") + 1);
+            dataContent = data.substring(data.indexOf("]") + 2);;
+
+            result = result + "Data Type is: " + dataType;
+        } catch (NullPointerException e) {
+            result = result + "Invalid Data Type";
+
+        }
+
+        if (dataContent != null) {
+            setServerGameState(dataType, dataContent);
+
+        }
+
+    }
+
+    private void setServerGameState(String dataType, String data) {
 
         switch (dataType) {
             case "[Username]":
-                //set player username, send data to client with success
+                BoardScene.updateOpponentUsername(data);
                 break;
+
             case "[Connect]":
                 //send exclusion list to client, they should create the board
                 break;
+
             case "[ServerMove]":
                 //server turn, send new exclusion list to client
                 break;
